a417f03b5a941fdf1128722ef7d00347c9100ae2
dglazier <Derek.Glazier@glasgow.ac.uk>
2018-11-26 09:46:23 +0000

    use lz4 so

---
+++ RunRoot/CleanROOT.C
@@ -0,0 +1,14 @@
+{
+ gSystem->Exec("rm $CLAS12TOOL/Hipo/*.so");
+ gSystem->Exec("rm $CLAS12TOOL/Hipo/*.d");
+ gSystem->Exec("rm $CLAS12TOOL/Hipo/*.pcm");
+
+ gSystem->Exec("rm $CLAS12TOOL/Banks/*.so");
+ gSystem->Exec("rm $CLAS12TOOL/Banks/*.d");
+ gSystem->Exec("rm $CLAS12TOOL/Banks/*.pcm");
+
+ gSystem->Exec("rm $CLAS12TOOL/RunRoot/*.so");
+ gSystem->Exec("rm $CLAS12TOOL/RunRoot/*.d");
+ gSystem->Exec("rm $CLAS12TOOL/RunRoot/*.pcm");
+
+}
